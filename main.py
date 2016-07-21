639989c5d46e2dd5a3cac281bac67164f23a589d
cm080691 <more08.chetan@gmail.com>
2016-07-21 00:59:00 +0000

    assignment 7

---
+++ main.py
@@ -0,0 +1,34 @@
+def filter_custom(l, f):
+    '''Filter a list using a function
+
+    Return a new list that contains all the elements e of l
+    for which f(e) is True
+
+    :param l: a list
+    :param f: a function that takes one argument and returns either True or False
+    '''
+    pass
+
+def map_custom(l, f):
+    '''Map a list using a function
+
+    Return a new list that applies f(e) for every element e in l
+
+    :param l: a list
+    :param f: a function that takes one argument and returns a value'''
+    pass
+
+def reduce_custom(l, f, starting_value):
+    '''Reduce a list using a reducer function and a starting value
+    return a single value that applies f(v, e) fro every
+    e in l from left to right. the initial value for v
+    should be starting _value, and subseuent values should be the
+    previously calculated value from f(v,e)
+
+    :param l: a list
+    :param f: a function that takes one argument and returns a value
+    :param starting_value: the begining value for the reducer function
+    computation
+
+    '''
+    pass
